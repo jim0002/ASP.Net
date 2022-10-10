15d1c0a55b61c2fb78a4674516731ee44b612fbb
jim0002 <jaminjim.aiub@gmail.com>
2022-10-10 15:06:20 +0600

    This is the full folder of my lab task 1

---
+++ LabTask1 (CV)/CV/Controllers/CVController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
